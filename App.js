30a4f42e7dabbaae48befe011719fe01a9aaa786
kiscdbc <kisc@dbc.dk>
2023-06-02 09:20:52 +0200

    pepper pig disappears when pulled into tiger

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useRef } from "react";
-import {
-  Animated,
-  Text,
-  View,
-  PanResponder,
-  StyleSheet,
-  Alert,
-  Dimensions,
-  Image,
-} from "react-native";
-import CustomImage from "./components/Image";
-import AnimatedImage from "./components/AnimatedImage";
+import React, { useRef, useState } from "react";
+import { Animated, View, PanResponder, StyleSheet, Image } from "react-native";
 
 export default function App() {
   const pan = useRef(new Animated.ValueXY()).current;
   const gurli = useRef(null);
-  // console.log("panResponder", pan.x, pan.y);
+  const backgroundImageRef = useRef(null);
+
+  const [isForegroundVisible, setForegroundVisible] = useState(true);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -30,34 +21,72 @@ export default function App() {
       ],
       { useNativeDriver: false }
     ),
-    onPanResponderRelease: (e, gesture) => {
-      // Reset the position of the foreground image using Animated.spring
-      Animated.spring(pan, {
-        toValue: { x: 0, y: 0 },
-        useNativeDriver: false,
-      }).start();
+    onPanResponderRelease: async (e, gesture) => {
+      // Check if the foreground image is on top of the background image
+      const foregroundImagePosition = await calculateImagePosition(gurli);
+      const backgroundImagePosition = await calculateImagePosition(
+        backgroundImageRef
+      );
+
+      if (
+        foregroundImagePosition.left >= backgroundImagePosition.left &&
+        foregroundImagePosition.top >= backgroundImagePosition.top &&
+        foregroundImagePosition.right <= backgroundImagePosition.right &&
+        foregroundImagePosition.bottom <= backgroundImagePosition.bottom
+      ) {
+        // Foreground image is on top of the background image, hide it
+        setForegroundVisible(false);
+      } else {
+        // Foreground image is not on top of the background image, reset its position
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
+        }).start();
+      }
     },
   });
+
+  const calculateImagePosition = (imageRef) => {
+    if (!imageRef || !imageRef.current) {
+      return { left: 0, top: 0, right: 0, bottom: 0 };
+    }
+
+    const { current: image } = imageRef;
+
+    return new Promise((resolve) => {
+      image.measure((x, y, width, height, left, top) => {
+        resolve({
+          left,
+          top,
+          right: left + width,
+          bottom: top + height,
+        });
+      });
+    });
+  };
 
   return (
     <View style={styles.container}>
       <Image
         source={require("./assets/tiger-with-books.png")}
         style={styles.backgroundImage}
+        ref={backgroundImageRef}
       />
-      <Animated.View
-        ref={gurli}
-        style={[
-          styles.foregroundImageContainer,
-          { transform: [{ translateX: pan.x }, { translateY: pan.y }] },
-        ]}
-        {...panResponder.panHandlers}
-      >
-        <Image
-          source={require("./assets/GurliGris.png")}
-          style={styles.foregroundImage}
-        />
-      </Animated.View>
+      {isForegroundVisible && (
+        <Animated.View
+          ref={gurli}
+          style={[
+            styles.foregroundImageContainer,
+            { transform: [{ translateX: pan.x }, { translateY: pan.y }] },
+          ]}
+          {...panResponder.panHandlers}
+        >
+          <Image
+            source={require("./assets/GurliGris.png")}
+            style={styles.foregroundImage}
+          />
+        </Animated.View>
+      )}
     </View>
   );
 }
